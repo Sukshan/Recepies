95e6046832efd273ffcd03a36b2e42430b4ae8d8
sukshan <sukshanj@gmail.com>
2023-08-23 19:52:34 +0530

    added connection to mongo cloud database

---
+++ Mern Recepie Project/server/src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors"
+import mongoose from "mongoose"  
+// mongoose is an orm for mongodb
+
+import dotenv from "dotenv"
+dotenv.config()
+
+const app = express()
+
+app.use(express.json());
+app.use(cors());
+
+
+//accessing the mongodb password from .env file
+const mongodbPassword = process.env.MONGODB_PASSWORD
+
+mongoose.connect(`mongodb+srv://sikandarsher4:${mongodbPassword}@recepies.8oaaxkb.mongodb.net/Recepies?retryWrites=true&w=majority`)
+
+app.listen(3001, ()=> console.log("server started!!!"))
