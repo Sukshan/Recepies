ebc940d7311da0fa79ed67819ab1257da799f6f8
sukshan <sukshanj@gmail.com>
2023-08-29 01:24:07 +0530

    added basic handler for registration

--- Mern Recepie Project/server/src/routes/Users.js
+++ Mern Recepie Project/server/src/routes/Users.js
@@ -1,10 +1,19 @@
 import express from "express"
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
+import { UserModel } from "../models/Users.js"
+
 
 const router = express.Router()
 
+router.post("/register", async(req, res) =>{
+    const {username, passoword} = req.body
 
+    const user = await UserModel.findOne({username});
+
+    res.json(user);
+})
+router.post("/login")
 
 
 
