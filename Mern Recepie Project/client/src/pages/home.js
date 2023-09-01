ffa0f2961f4b2c663d400f4c89fd36e2e3123aae
sukshan <sukshanj@gmail.com>
2023-09-01 23:33:43 +0530

    started working on home page

--- Mern Recepie Project/client/src/pages/home.js
+++ Mern Recepie Project/client/src/pages/home.js
@@ -1,10 +1,30 @@
-import React from "react"
+import { useEffect, useState } from "react";
+import { recipesRouter } from "../../../server/src/routes/recipes";
 export const Home = () => {
-    return(
+    const [recipe, setRecipes] = useState([])
+
+    useEffect(() => {
+
+        const fetchRecipe = async () => {
+            try{
+                const response = await axios.get("http://localhost:3001/recipes")
+                setRecipes(response.data)
+            }
+            catch(err){
+                console.error(err)
+            }
+        }
+        fetchRecipe()
+
+    }, [])
+    return (
         <div>
-            <h1>
-                home
-            </h1>
+            <h2>Recipes</h2>
+            <ul>
+                {recipesRouter.map((recipe) => (
+                    <li key={recipe._id}></li>
+                ))}
+            </ul>
         </div>
     )
 }
