92b158a9cc45f109c5613c72068fdc6e3181c49b
sukshan <sukshanj@gmail.com>
2023-09-03 23:53:54 +0530

    template for home page

--- Mern Recepie Project/client/src/pages/home.js
+++ Mern Recepie Project/client/src/pages/home.js
@@ -22,7 +22,18 @@ export const Home = () => {
             <h2>Recipes</h2>
             <ul>
                 {recipesRouter.map((recipe) => (
-                    <li key={recipe._id}></li>
+                    <li key={recipe._id}>
+                        <div>
+                            <h2>
+                                {recipe.name}
+                            </h2>
+                        </div>
+                        <div>
+                            <p>{recipe.instructions}</p>
+                        </div>
+                        <img src={recipe.imageUrl} alt= {recipe.name} />
+                        <p> Cooking Time: {recipe.cookingTime} (minutes)</p>
+                    </li>
                 ))}
             </ul>
         </div>
